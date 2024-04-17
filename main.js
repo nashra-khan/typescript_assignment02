6076c5892871ff9c99d2160d85e638922ccc9c6e
Nashra Khan <biyak973@gmail.com>
2024-04-18 02:42:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Eric";
+let message = (`Hello ${personName}, would you like to learn some python today?`);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let personName : string = "Eric";
+let message : string = (`Hello ${personName}, would you like to learn some python today?`);
+console.log(message)
